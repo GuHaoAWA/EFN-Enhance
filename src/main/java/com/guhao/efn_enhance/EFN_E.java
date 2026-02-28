5f11c475d38804b0c95b5069d75760750207b354
GuHaoAWA <1441857228@qq.com>
2026-02-28 12:20:59 +0800

    着色器

--- src/main/java/com/guhao/efn_enhance/EFN_E.java
+++ src/main/java/com/guhao/efn_enhance/EFN_E.java
@@ -16,7 +16,6 @@ import net.minecraftforge.event.AddPackFindersEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
